be2862d271b60bee502c7c1a10b89ecb347988ab
Lying <7865059+Lyinginbedmon@users.noreply.github.com>
2024-10-26 18:57:49 +0100

    Resolved Issue #12
    Moved parented entity discrimination into the tickEntity function instead of overriding the tickEntities function, which resulted in unintended event call negation

--- common/src/main/java/com/lying/mixin/ClientWorldMixin.java
+++ common/src/main/java/com/lying/mixin/ClientWorldMixin.java
@@ -1,7 +1,6 @@
 package com.lying.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,34 +10,10 @@ import com.lying.entity.IParentedEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.profiler.Profiler;
-import net.minecraft.world.EntityList;
 
 @Mixin(ClientWorld.class)
 public class ClientWorldMixin extends WorldMixin
 {
-	@Shadow
-	final EntityList entityList = new EntityList();
-	
-	@Shadow
-	public void tickEntity(Entity entity) { }
-	
-	@Inject(method = "tickEntities()V", at = @At("HEAD"), cancellable = true)
-	private void whc$tickEntities(final CallbackInfo ci)
-	{
-		ci.cancel();
-		
-		Profiler profiler = getProfiler();
-		profiler.push("entities");
-		entityList.forEach(entity -> {
-			if(entity.isRemoved() || entity.hasVehicle() || entity instanceof IParentedEntity && ((IParentedEntity)entity).hasParent())
-				return;
-			tickEntity(this::tickEntity, entity);
-		});
-		profiler.pop();
-		tickBlockEntities();
-	}
-	
 	@Inject(method = "tickEntity(Lnet/minecraft/entity/Entity;)V", at = @At("TAIL"))
 	private void whc$tickEntity(Entity entity, final CallbackInfo ci)
 	{
--- common/src/main/java/com/lying/mixin/WorldMixin.java
+++ common/src/main/java/com/lying/mixin/WorldMixin.java
@@ -3,21 +3,23 @@ package com.lying.mixin;
 import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.lying.entity.IParentedEntity;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.World;
 
 @Mixin(World.class)
 public class WorldMixin
 {
-	@Shadow
-	public Profiler getProfiler() { return null; }
-	
-	@Shadow
-	public <T extends Entity> void tickEntity(Consumer<T> tickConsumer, T entity) { }
-	
-	@Shadow
-	protected void tickBlockEntities() { }
+	@SuppressWarnings("rawtypes")
+	@Inject(method = "tickEntity(Ljava/util/function/Consumer;Lnet/minecraft/entity/Entity;)V", at = @At("HEAD"), cancellable = true)
+	private void whc$tickEntity(Consumer tickConsumer, Entity entity, final CallbackInfo ci)
+	{
+		if(entity.getWorld().isClient() && entity instanceof IParentedEntity && ((IParentedEntity)entity).hasParent())
+			ci.cancel();
+	}
 }
